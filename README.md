c806c7afe21b4e7b21f0c7719af5589857bfa45f
Xinqian liu <crystalynliu@163.com>
2017-03-21 15:50:23 +0800

    add script for package files that is font and css

--- README.md
+++ README.md
@@ -11,3 +11,9 @@
  5. 浏览器中就可以预览 svg 图片并可以看到对应的 codepoint 和 class-name
  6. 使用 `<i class={class-name}></i>`标签在 html 中加入 svg 图片
  
+---
+
+ ####注意：
+ 
+ 1. 如果只想编译 svg 文件，并不想启动 server， 使用`npm run icon` 命令，在 dist 文件夹下可以找到编译后的 font 和 CSS 文件。
+ 2. 运行 `./packagefile.sh` 脚本文件，压缩打包编译后的 font 以及 CSS 文件。
---
+++ packagefile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "1. first delete fontLibrary.zip"
+
+rm -rf dist/fontLibrary.zip
+
+echo "2. Starting build new fontLib with new font folder and css file!"
+
+mkdir fontLibrary
+cp -R dist/font fontLibrary/
+cp dist/iconfont.css fontLibrary/
+
+echo "3. Starting zip fontLib to fontLib.zip. Please input password '1' and send to your huawei email!"
+
+zip -e -r dist/fontLibrary.zip fontLibrary
+
+rm -rf fontLibrary
+
+echo "4. finished! Find fontLibrary.zip in folder dist"
